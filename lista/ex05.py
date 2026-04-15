cb471cbf682644613192d3e551d2a096d9f68fad
gilberto <gilbertogulart70@gmail.com>
2026-04-15 19:59:59 -0300

    exercicio 5: Boletim, média e conceito.

---
+++ lista/ex05.py
@@ -0,0 +1,24 @@
+nota1 = float(input("Digite a sua nota: "))
+nota2 = float(input("Digite a sua nota: "))
+
+average = (nota1 + nota2) / 2
+
+def showResult(n1: float, n2: float, m: float, c: str, msg: str):
+    print(f"""
+        ----------------- BOLETIM -----------------
+            Nota 1 :{n1}            Nota 2: {n2}
+            Média: {m}              conceito: {c}
+                           {msg}
+         """)
+
+if average >= 9.00 and average <= 10.00:
+    showResult(nota1, nota2, average, 'A', "APROVADO")
+
+elif average >= 7.5:
+    showResult(nota1, nota2, average, 'B', "APROVADO")
+elif average >= 6.00:
+    showResult(nota1, nota2, average, 'C', "APROVADO")
+elif average >= 4.00:
+    showResult(nota1, nota2, average, 'D', "REPROVADO")
+elif average < 4:
+    showResult(nota1, nota2, average, 'E', "REPROVADO")
