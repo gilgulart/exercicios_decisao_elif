0c9ff67b1712ba900c6a61de08a174d7495b089f
gilberto <gilbertogulart70@gmail.com>
2026-04-15 19:32:30 -0300

    exercicio 3: matutino, vespertino, noturno.

---
+++ lista/ex03.py
@@ -0,0 +1,16 @@
+print("""--------Em qual turno você estuda?--------
+         M- matutino | V- vespertino | N- noturno   """)
+
+enter = input("> ").upper()
+shift = ["M", "V", "N"]
+
+if enter in shift:
+    if enter == shift[0]:
+        print("Bom dia!")
+    elif enter == shift[1]:
+        print("Boa tarde!")
+    elif enter == shift[2]:
+        print("Boa Noite!")
+else:
+    print("Valor inválido!")
+    
