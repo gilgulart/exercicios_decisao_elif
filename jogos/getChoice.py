3737a5f97fa4a58f8c311bde3e4660ae37aba473
gilberto <gilbertogulart70@gmail.com>
2026-04-17 12:53:16 -0300

    Estruturando o RPG

--- jogos/getChoice.py
+++ jogos/getChoice.py
@@ -1,6 +1,8 @@
 def getChoice():
-        choice = int(input("Digite o número da sua escolha: "))
-        if choice <= 0 or choice > 3:
-            print("Digite um valor válido")
-        else:
+    while True:
+        try:
+            choice = int(input("Digite o número da sua escolha: "))
+            # if choice <= 0 or choice > 3:
             return choice
+        except:
+            print("Digite um valor válido")
--- jogos/rpg_game.py
+++ jogos/rpg_game.py
@@ -1,15 +1,26 @@
 from getChoice import getChoice
 from introHistory import introHistory
 from history import history, toFight
+from colorama import Fore, Style, init
+import pyfiglet
+import os
 
+title = pyfiglet.figlet_format("""
+                               Mundo de
+                                Eldoria """)
+print(title)
+
+init()
   
 introHistory()
 choice = getChoice()
+os.system("cls")
 
 
 if choice == 1:
   history("AK-47","dragão")
   getChoice()
+  os.system("cls")
   
   if choice == 1:
     # luta
