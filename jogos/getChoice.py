ad1633b04c5183f1bca7a4afe07b30a2f12e5139
gilberto <gilbertogulart70@gmail.com>
2026-04-16 22:05:06 -0300

    Estruturando o RPG

--- jogos/getChoice.py
+++ jogos/getChoice.py
@@ -1,5 +1,4 @@
 def getChoice():
-    while True:
         choice = int(input("Digite o número da sua escolha: "))
         if choice <= 0 or choice > 3:
             print("Digite um valor válido")
--- jogos/rpg_game.py
+++ jogos/rpg_game.py
@@ -1,22 +1,20 @@
 from getChoice import getChoice
 from introHistory import introHistory
-  
-choice = getChoice()
-  
-  
-if choice == 1:
-      print("""
-            
-            
-            Você encontra um Lobo gigante!
-            suas pernas tremem na presença desta criatura mítica
-            e agora restam poucas opções:
-        ---------------------------------------------------------------
-            1. Lutar com sua espada
-            2. Tentar fugir
-            3. Oferecer comida ao lobo
-            """)
-      getChoice()
+from history import history, toFight
+
   
 introHistory()
-mainHistory()
+choice = getChoice()
+
+
+if choice == 1:
+  history("AK-47","dragão")
+  getChoice()
+  
+  if choice == 1:
+    # luta
+    toFight()
+  # elif choice == 2:
+  #   # fuga
+  # else:
+  #   # oferecer comida  
